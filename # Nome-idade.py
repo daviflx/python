fc8e7e2b2aa777bc834dd903b4c55d1e75115b31
Davi Félix <davi10felixs@gmail.com>
2024-08-29 11:57:49 -0300

    Add files via upload

---
+++ # Nome-idade.py
@@ -0,0 +1,15 @@
+# Nome-idade
+# atribuição de variáveis
+nome = "Davi\n"
+idade = 16
+altura = 1,79
+
+#exibir mensagem na tela
+print(" nome: ", nome," idade: ", idade, " altura : ", altura)
+#formatação de string
+print(f"nome: {nome} idade: {idade} altura: {altura }")
+nome = (input("Digite o seu nome: "))
+idade = int(input("Digite sua idade: "))
+altura = float(input("Digite a sua altura: "))
+
+print(f"nome: {nome}idade: {idade} altura: {altura}")
