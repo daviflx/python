b760c7bfc08997c4984ac0bf848d5c51e15ce95a
Davi Félix <davi10felixs@gmail.com>
2024-09-10 11:25:39 -0300

    Add files via upload

---
+++ davé/06-operadores-aritmeticos.py
@@ -0,0 +1,21 @@
+# Operadores Aritmeticos
+# Soma
+soma = 10 + 5
+print(soma)
+# subtracao
+subtracao = 10 - 5
+print(subtracao)
+# Multiplicacao
+multiplicacao = 10 * 5
+print(multiplicacao)
+# Divisao
+divisao = 10 / 5
+print(divisao)
+# Divisao inteira
+divisao_inteira = 10 // 5
+print(divisao_inteira)
+# Exponenciacao = 10 ** 2
+print(exponenciacao)
+# Modulo / Resto da divisao
+modulo = 10 % 5
+print(modulo)
