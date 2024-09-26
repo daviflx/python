b9c827df21e08c682db3fb2e9ff0584367c17d54
Davi Félix <davi10felixs@gmail.com>
2024-09-26 10:08:44 -0300

    Add files via upload

---
+++ davé/aula-03.py
@@ -0,0 +1,16 @@
+# Variáveis em Python
+# Não há necessidade de declará-las no início do programa
+# Basta criar no meio do algaritmo com a atribuição
+
+Nome = "Davi"
+print(Nome) #Imprime a string "Davi"
+print(type (Nome)) #Imprime o tipo da variável, string
+Idade = 16
+print(Idade) #imprime o número 16
+print(type (Idade)) #Imprime o tipo da variável, que é int
+Altura = 1.80
+print(Altura) # Imprime o número 1.80
+print(type(Altura)) #Imprime o tipo da variável, que é Float
+teste = True
+print(teste) # Imprime o valor True
+print(type(teste)) #Imprime o tipo da variável, que é Bool
